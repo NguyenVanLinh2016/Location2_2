35bb07544c78331cd04bacd9bcbc0aa31eb5dd87
thuandt <thuandtps03975@fpt.edu.vn>
2017-05-02 15:56:26 +0700

    test1+push

--- app/src/main/java/dev/linhnv/location2_2/MainActivity.java
+++ app/src/main/java/dev/linhnv/location2_2/MainActivity.java
@@ -23,7 +23,7 @@ import com.google.firebase.database.FirebaseDatabase;
 //1
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener{
     //2. khai bao anh xa view
-    //test1
+    //test1 + push
 
     private final String TAG = "LaurenTestApp";
     private TextView txtLatitude;
